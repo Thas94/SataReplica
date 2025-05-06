3b17dfa4c5e976bc33de9d00002d68be4b5a919c
Thas94 <thasmalematja@gmail.com>
2025-05-06 21:37:00 +0200

    Project setup (Added tailwindcc, primevue and posscss)

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+  css: ["@/assets/styles/tailwind.css"],
+  primevue: {
+      options: {
+          theme: 'none'
+      }
+  },
+  postcss: {
+    plugins: {
+        "postcss-import": {},
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <h1>Entry point</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
